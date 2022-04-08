0fb91067596b198d19942724752ccf3f97030e29
Victorsousa98 <victorsousa708@gmail.com>
2022-04-08 02:12:30 -0300

    adiciona persistencia dos dados no localStorage

--- js/main.js
+++ js/main.js
@@ -1,32 +1,45 @@
 const form = document.querySelector('#novoItem');
 const lista = document.querySelector('#lista');//pega a lista
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
+itens.forEach(elemento => {
+    criaElemento(elemento);
+});
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();//previne o comportamento padrão do formulário
-    const nomeDoElemento = event.target.elements["nome"].value;//pega o valor do input nome
-    const quantidadeDoElemento = event.target.elements["quantidade"].value;//pega o valor do input quantidade
-    
+    const nomeDoElemento = event.target.elements["nome"];//pega o valor do input nome
+    const quantidadeDoElemento = event.target.elements["quantidade"];//pega o valor do input quantidade
+
+    const itemAtual = {
+        nome: nomeDoElemento.value,
+        quantidade: quantidadeDoElemento.value
+    }
+      
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual);
+
+    localStorage.setItem("itens", JSON.stringify(itens));//salva o item no localStorage e converte para string
   
-    criaElemento(nomeDoElemento, quantidadeDoElemento);
+    nomeDoElemento.value = "";
+    quantidadeDoElemento.value = "";
+    console.log(itens)
 });
 
 //cria o elemento na tabela
-function criaElemento(nomeDoElemento, quantidadeDoElemento) {
-    //<li class="item"><strong>7</strong>roupas</li>
-
-    console.log(nomeDoElemento, quantidadeDoElemento);
+function criaElemento(item) {
 
     const novoItem = document.createElement('li');//cria o elemento li
     novoItem.classList.add('item');//adiciona a classe item
 
     const quantidadeItem = document.createElement('strong');//cria o elemento strong
-    quantidadeItem.innerHTML = quantidadeDoElemento;//adiciona o texto
+    quantidadeItem.innerHTML = item.quantidade//adiciona o texto
 
     novoItem.appendChild(quantidadeItem);//adiciona o elemento strong ao elemento li
-    novoItem.innerHTML += nomeDoElemento;//adiciona o texto ao elemento li
+    novoItem.innerHTML += item.nome;//adiciona o texto ao elemento li
 
     lista.appendChild(novoItem);//adiciona o elemento li a lista
 
-    console.log(novoItem)
+
 }
